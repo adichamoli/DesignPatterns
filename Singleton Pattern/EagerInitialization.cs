d2c4538e75e7670566c99289f1096111a2d29e6f
adichamoli <50514555+adichamoli@users.noreply.github.com>
2021-02-03 00:02:12 +0530

    DesignPatterns

---
+++ Singleton Pattern/EagerInitialization.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace EagerInitialization
+{
+    public sealed class EagerInitializedSingleton
+    {
+        private static readonly EagerInitializedSingleton instance = new EagerInitializedSingleton();
+
+        //private constructor to avoid client applications to use constructor
+        private EagerInitializedSingleton() 
+        {
+            Console.WriteLine("Hello Singleton");
+        }
+
+        public static EagerInitializedSingleton getInstance
+        {
+            get
+            {
+                return instance;
+            } 
+        }
+
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    class EagerInitialization
+    {
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(
+                () => PrintTeacherDetails(),
+                () => PrintStudentdetails()
+                );
+        }
+        private static void PrintTeacherDetails()
+        {
+            EagerInitializedSingleton fromTeacher = EagerInitializedSingleton.getInstance;
+            fromTeacher.PrintDetails("From Teacher");
+        }
+        
+        private static void PrintStudentdetails()
+        {
+            EagerInitializedSingleton fromStudent = EagerInitializedSingleton.getInstance;
+            fromStudent.PrintDetails("From Student");
+        }
+    }
+}
