18691ff8784c6e0cabf3323d46a0c6f985af49bc
adichamoli <50514555+adichamoli@users.noreply.github.com>
2021-02-04 00:20:54 +0530

    DesignPatterns

---
+++ Creational Design Pattern/Builder Pattern/BuilderPatternImpl/VehicleCreator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuilderPatternImpl
+{
+    public class VehicleCreator
+    {
+        private readonly IVehicleBuilder objBuilder;
+
+        public VehicleCreator(IVehicleBuilder builder)
+        {
+            objBuilder = builder;
+        }
+
+        public void CreateVehicle()
+        {
+            objBuilder.SetModel();
+            objBuilder.SetEngine();
+            objBuilder.SetBody();
+            objBuilder.SetTransmission();
+            objBuilder.SetAccessories();
+        }
+
+        public Vehicle GetVehicle()
+        {
+            return objBuilder.GetVehicle();
+        }
+    }
+}
