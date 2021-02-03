d72e80caaf6517b7267df0ca19846d779fa2a9df
adichamoli <50514555+adichamoli@users.noreply.github.com>
2021-02-03 20:10:49 +0530

    DesignPatterns

---
+++ Singleton Pattern/LazyInitializationLibrary.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace LazyInitializedSingletonImpl
+{
+    public sealed class LazyInitializedSingleton
+    {
+        private static readonly Lazy<LazyInitializedSingleton> lazy =
+                                new Lazy<LazyInitializedSingleton>
+                                (() => new LazyInitializedSingleton());
+
+        public static LazyInitializedSingleton getInstance 
+        { 
+            get 
+            { 
+                return lazy.Value; 
+            } 
+        }
+
+        private LazyInitializedSingleton()
+        {
+            Console.WriteLine("Hello Singleton");
+        }
+
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+    }
+    class LazyInitializationLibrary
+    {
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(
+                () => PrintTeacherDetails(),
+                () => PrintStudentdetails()
+                );
+        }
+        private static void PrintTeacherDetails()
+        {
+            LazyInitializedSingleton fromTeacher = LazyInitializedSingleton.getInstance;
+            fromTeacher.PrintDetails("From Teacher");
+        }
+
+        private static void PrintStudentdetails()
+        {
+            LazyInitializedSingleton fromStudent = LazyInitializedSingleton.getInstance;
+            fromStudent.PrintDetails("From Student");
+        }
+    }
+}
