a037e89399261cea9da4d1a00820f37d979d43a5
adichamoli <50514555+adichamoli@users.noreply.github.com>
2021-02-04 13:19:07 +0530

    DesignPatterns

---
+++ Creational Design Pattern/Abstract Factory Pattern/AbstractFactoryImpl/Lion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractFactoryImpl
+{
+    public class Lion : Animal
+    {
+        public string speak()
+        {
+            return "Roar";
+        }
+    }
+}
