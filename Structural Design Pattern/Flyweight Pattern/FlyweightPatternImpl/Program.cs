cfe9d4fa7bc078201a7fe68256ca66e5f6777784
adichamoli <50514555+adichamoli@users.noreply.github.com>
2021-02-06 19:43:52 +0530

    DesignPatterns

---
+++ Structural Design Pattern/Flyweight Pattern/FlyweightPatternImpl/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FlyweightPatternImpl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string document = "AAZZBBZB";
+            char[] chars = document.ToCharArray();
+
+            CharacterFactory factory = new CharacterFactory();
+
+            int pointSize = 10;
+
+            foreach (char c in chars)
+            {
+                pointSize++;
+                Character character = factory.GetCharacter(c);
+                character.Display(pointSize);
+            }
+        }
+    }
+}
