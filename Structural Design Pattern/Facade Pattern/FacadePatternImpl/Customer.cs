c7abc25b863d1ac4eab8efc5aef0aac9cbf3e8f2
adichamoli <50514555+adichamoli@users.noreply.github.com>
2021-02-06 19:13:28 +0530

    DesignPatterns

---
+++ Structural Design Pattern/Facade Pattern/FacadePatternImpl/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FacadePatternImpl
+{
+    class Customer
+    {
+        private string _name;
+
+        public Customer(string name)
+        {
+            this._name = name;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+    }
+}
