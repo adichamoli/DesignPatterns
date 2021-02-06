3f5c25aea7fdd521b5840b9fde93c4c3abc12b71
adichamoli <50514555+adichamoli@users.noreply.github.com>
2021-02-06 17:14:51 +0530

    DesignPatterns

---
+++ Structural Design Pattern/Bridge Pattern/BridgePatternImpl/RefinedAbstraction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgePatternImpl
+{
+    class Customers : CustomersBase
+    {
+        public Customers(string group)
+          : base(group)
+        {
+        }
+
+        public override void ShowAll()
+        {
+            Console.WriteLine();
+            Console.WriteLine("------------------------");
+            base.ShowAll();
+            Console.WriteLine("------------------------");
+        }
+    }
+}
