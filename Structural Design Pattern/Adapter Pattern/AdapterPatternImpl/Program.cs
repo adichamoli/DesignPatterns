e962b83dd143e0cbdb68df209180808d4ae28adb
adichamoli <50514555+adichamoli@users.noreply.github.com>
2021-02-06 16:48:14 +0530

    DesignPatterns

---
+++ Structural Design Pattern/Adapter Pattern/AdapterPatternImpl/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AdapterPatternImpl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Compound unknown = new Compound("Unknown");
+            unknown.Display();
+
+            Compound water = new RichCompound("Water");
+            water.Display();
+
+            Compound benzene = new RichCompound("Benzene");
+            benzene.Display();
+
+            Compound ethanol = new RichCompound("Ethanol");
+            ethanol.Display();
+        }
+    }
+}
