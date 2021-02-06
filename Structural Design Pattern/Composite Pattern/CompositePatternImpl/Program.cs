e765260632031448f84fa503cff05e9708abe126
adichamoli <50514555+adichamoli@users.noreply.github.com>
2021-02-06 17:48:38 +0530

    DesignPatterns

---
+++ Structural Design Pattern/Composite Pattern/CompositePatternImpl/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CompositePatternImpl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CompositeElement root = new CompositeElement("Picture");
+            root.Add(new PrimitiveElement("Red Line"));
+            root.Add(new PrimitiveElement("Blue Circle"));
+            root.Add(new PrimitiveElement("Green Box"));
+
+            CompositeElement comp = new CompositeElement("Two Circles");
+            comp.Add(new PrimitiveElement("Black Circle"));
+            comp.Add(new PrimitiveElement("White Circle"));
+            root.Add(comp);
+
+            PrimitiveElement pe = new PrimitiveElement("Yellow Line");
+            root.Add(pe);
+            root.Remove(pe);
+
+            root.Display(1);
+        }
+    }
+}
