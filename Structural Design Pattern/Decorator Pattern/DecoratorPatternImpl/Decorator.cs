2dfeffdaf386f55d4e2e739f8d4c210b1fef7f0d
adichamoli <50514555+adichamoli@users.noreply.github.com>
2021-02-06 18:52:35 +0530

    DesignPatterns

---
+++ Structural Design Pattern/Decorator Pattern/DecoratorPatternImpl/Decorator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecoratorPatternImpl
+{
+    abstract class Decorator : LibraryItem
+    {
+        protected LibraryItem libraryItem;
+
+        public Decorator(LibraryItem libraryItem)
+        {
+            this.libraryItem = libraryItem;
+        }
+
+        public override void Display()
+        {
+            libraryItem.Display();
+        }
+    }
+}
