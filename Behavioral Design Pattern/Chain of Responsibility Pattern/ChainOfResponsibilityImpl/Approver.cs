d020e65b4681238825296a9c80ce30e99a6f0ccf
adichamoli <50514555+adichamoli@users.noreply.github.com>
2021-02-07 13:14:54 +0530

    DesignPatterns

---
+++ Behavioral Design Pattern/Chain of Responsibility Pattern/ChainOfResponsibilityImpl/Approver.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChainOfResponsibilityImpl
+{
+    abstract class Approver
+    {
+        protected Approver successor;
+
+        public void SetSuccessor(Approver successor)
+        {
+            this.successor = successor;
+        }
+
+        public abstract void ProcessRequest(Purchase purchase);
+    }
+}
