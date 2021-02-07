b535b7caee30c28f6fcd3fef905a758e983cb547
adichamoli <50514555+adichamoli@users.noreply.github.com>
2021-02-07 13:48:34 +0530

    DesignPatterns

---
+++ Behavioral Design Pattern/Interpreter Pattern/InterpreterPatternImpl/Context.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterpreterPatternImpl
+{
+    class Context
+    {
+        private string _input;
+        private int _output;
+
+        public Context(string input)
+        {
+            this._input = input;
+        }
+
+        public string Input
+        {
+            get { return _input; }
+            set { _input = value; }
+        }
+
+        public int Output
+        {
+            get { return _output; }
+            set { _output = value; }
+        }
+    }
+}
