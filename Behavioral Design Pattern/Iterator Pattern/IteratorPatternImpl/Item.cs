c47f67b415b80b62677af3db16e90ca60617cd89
adichamoli <50514555+adichamoli@users.noreply.github.com>
2021-02-08 19:48:43 +0530

    DesignPatterns

---
+++ Behavioral Design Pattern/Iterator Pattern/IteratorPatternImpl/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorPatternImpl
+{
+    class Item
+    {
+        private string _name;
+
+        public Item(string name)
+        {
+            this._name = name;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+    }
+}
