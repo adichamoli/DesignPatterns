f8b1987f9eaf299305cf059e02f1e3654139ea01
adichamoli <50514555+adichamoli@users.noreply.github.com>
2021-02-08 21:11:58 +0530

    DesignPatterns

---
+++ Behavioral Design Pattern/Memento Pattern/MementoPatternImpl/ProspectMemory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MementoPatternImpl
+{
+    class ProspectMemory
+    {
+        private Memento _memento;
+
+        public Memento Memento
+        {
+            set { _memento = value; }
+            get { return _memento; }
+        }
+    }
+}
