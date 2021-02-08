d8e485dd7ebca38a6ab0843baa2dd73e69317bcd
adichamoli <50514555+adichamoli@users.noreply.github.com>
2021-02-08 23:07:57 +0530

    DesignPatterns

---
+++ Behavioral Design Pattern/Observer Pattern/ObserverPatternImpl/IInvestor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObserverPatternImpl
+{
+    interface IInvestor
+    {
+        void Update(Stock stock);
+    }
+}
