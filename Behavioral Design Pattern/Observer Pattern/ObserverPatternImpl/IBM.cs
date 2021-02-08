9d85c8246a6e745a59ba90597c19cedfc4c4cdeb
adichamoli <50514555+adichamoli@users.noreply.github.com>
2021-02-08 23:07:48 +0530

    DesignPatterns

---
+++ Behavioral Design Pattern/Observer Pattern/ObserverPatternImpl/IBM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObserverPatternImpl
+{
+    class IBM : Stock
+    {
+        public IBM(string symbol, double price)
+          : base(symbol, price)
+        {
+        }
+    }
+}
