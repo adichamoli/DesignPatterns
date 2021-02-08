ec9ce56541381d58e0b867b6a6783ca911f666ee
adichamoli <50514555+adichamoli@users.noreply.github.com>
2021-02-08 20:21:54 +0530

    DesignPatterns

---
+++ Behavioral Design Pattern/Mediator Pattern/MeditorPatternImpl/Beatle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeditorPatternImpl
+{
+    class Beatle : Participant
+    {
+        public Beatle(string name)
+          : base(name)
+        {
+        }
+
+        public override void Receive(string from, string message)
+        {
+            Console.Write("To a Beatle: ");
+            base.Receive(from, message);
+        }
+    }
+}
