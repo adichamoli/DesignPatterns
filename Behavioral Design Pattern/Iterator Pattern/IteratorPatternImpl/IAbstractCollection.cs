6eb8b8a729245aaefeea555e6150f4d5cd12dc71
adichamoli <50514555+adichamoli@users.noreply.github.com>
2021-02-08 19:48:14 +0530

    DesignPatterns

---
+++ Behavioral Design Pattern/Iterator Pattern/IteratorPatternImpl/IAbstractCollection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorPatternImpl
+{
+    interface IAbstractCollection
+    {
+        Iterator CreateIterator();
+    }
+}
