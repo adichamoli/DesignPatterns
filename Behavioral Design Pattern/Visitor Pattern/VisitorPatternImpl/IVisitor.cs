e88aa607232d8109f7785a3d03a8c5215d838472
adichamoli <50514555+adichamoli@users.noreply.github.com>
2021-02-09 23:43:35 +0530

    DesignPatterns

---
+++ Behavioral Design Pattern/Visitor Pattern/VisitorPatternImpl/IVisitor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VisitorPatternImpl
+{
+    interface IVisitor
+    {
+        void Visit(Element element);
+    }
+}
