7bd4eef1841ed81c950321f1a5b17999c8705b81
adichamoli <50514555+adichamoli@users.noreply.github.com>
2021-02-09 23:44:00 +0530

    DesignPatterns

---
+++ Behavioral Design Pattern/Visitor Pattern/VisitorPatternImpl/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace VisitorPatternImpl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employees e = new Employees();
+            e.Attach(new Clerk());
+            e.Attach(new Director());
+            e.Attach(new President());
+
+            e.Accept(new IncomeVisitor());
+            e.Accept(new VacationVisitor());
+        }
+    }
+}
