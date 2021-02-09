27143cf13f8f037a5b4ec1b74dbf1138561fb42c
adichamoli <50514555+adichamoli@users.noreply.github.com>
2021-02-09 21:12:49 +0530

    DesignPatterns

---
+++ Behavioral Design Pattern/Strategy Pattern/StrategyPatternImpl/MergeSort.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StrategyPatternImpl
+{
+    class MergeSort : SortStrategy
+    {
+        public override void Sort(List<string> list)
+        {
+            Console.WriteLine("MergeSorted list ");
+        }
+    }
+}
