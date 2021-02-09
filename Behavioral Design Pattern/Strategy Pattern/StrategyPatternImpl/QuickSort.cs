5b4f281cce0e5cea106aa11242fb98850241b7b3
adichamoli <50514555+adichamoli@users.noreply.github.com>
2021-02-09 21:18:34 +0530

    DesignPatterns

---
+++ Behavioral Design Pattern/Strategy Pattern/StrategyPatternImpl/QuickSort.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StrategyPatternImpl
+{
+    class QuickSort : SortStrategy
+    {
+        public override void Sort(List<string> list)
+        {
+            list.Sort();
+
+            Console.WriteLine("QuickSorted list ");
+        }
+    }
+}
