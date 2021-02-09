55d2b19eb069a2a13cbc045d767d6e9180efbfb4
adichamoli <50514555+adichamoli@users.noreply.github.com>
2021-02-09 23:21:49 +0530

    DesignPatterns

---
+++ Behavioral Design Pattern/Template Method Pattern/TemplateMethodPatternImpl/Client.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TemplateMethodPatternImpl
+{
+    class Client
+    {
+        public static void ClientCode(AbstractClass abstractClass)
+        {
+            abstractClass.TemplateMethod();
+        }
+    }
+}
